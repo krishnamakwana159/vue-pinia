381ae715d0791eed62f6c23c78a03f32fb24c25d
Swasti Bhagora <bhagorasurekhaben@gmail.com>
2024-03-31 22:22:46 +0530

    Signing off

--- src/modules/admin/__tests__/ProductForm.spec.ts
+++ src/modules/admin/__tests__/ProductForm.spec.ts
@@ -1,15 +1,23 @@
-import { mount } from '@vue/test-utils'
+import { mount, flushPromises } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import ProductForm from '../components/ProductForm.vue'
-import { createTestingPinia } from '@pinia/testing'
+import router from '@/router'
 
 describe('Product Form', () => {
   it('Validation of Product Form', async () => {
+    router.push('/admin')
+
+    await router.isReady()
+
     const wrapper = mount(ProductForm, {
-      global: {}
+      global: {
+        plugins: [router]
+      }
     })
 
     const inputArr = wrapper.findAll('input')
+
+    await flushPromises()
 
     console.log(inputArr)
 
